db730c79199f116857d826a7cdffe01058907fe5
smhrd <smhrd@220.80.33.84>
2022-04-15 16:28:32 +0900

    이연진-세번째 커밋

--- GitTest/src/main/java/Ex01.java
+++ GitTest/src/main/java/Ex01.java
@@ -4,6 +4,7 @@ public class Ex01 {
 	public static void main(String[] args) {
 		System.out.println("첫번째 커밋");
 		System.out.println("두번째 커밋 - 이예원");
+		System.out.println("세번째 커밋 - 이연진!");
 	}
 
 }
