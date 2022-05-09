d6c3543ee3394272fcd9d4068321b2cfa040ed4b
smhrd <smhrd@220.80.33.84>
2022-05-09 14:12:23 +0900

    이연진
    추가파일

---
+++ MenuRecommendation/src/main/java/MT/controller/JoinService.java
@@ -0,0 +1,47 @@
+package MT.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import MT.model.MemberDAO;
+import MT.model.MemberVO;
+
+@WebServlet("/JoinService")
+public class JoinService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 0. 인코딩
+		request.setCharacterEncoding("utf-8");
+		
+		// 1. 파라미터 수집
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String nickname = request.getParameter("nickname");
+		// vo로 묶기
+		MemberVO vo = new MemberVO(id, pw, nickname);
+		
+		// 2. DAO메서드 사용
+		MemberDAO dao = new MemberDAO();
+		int cnt = dao.join(vo);
+		
+		// 3. 회원가입 성공/실패 판단
+		if(cnt>0) {
+			// 성공 -> main으로
+			response.sendRedirect("main.html");
+		}else {
+			// 실패 -> join으로
+			response.sendRedirect("join.html");
+		}
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ MenuRecommendation/src/main/java/MT/model/MemberDAO.java
@@ -0,0 +1,65 @@
+package MT.model;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MemberDAO {
+
+
+	// Connection pool만들기
+	private static SqlSessionFactory sqlSessionFactory;
+
+	static {
+		try {
+			// Connection pool설정
+			String resource = "MT/mapper/config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// ==============================================================
+
+	// join 메서드
+	public int join(MemberVO vo) {
+
+		// 1. SQL Session 빌려오기
+		SqlSession session = sqlSessionFactory.openSession(true);
+		
+		// 2. Mapper.xml에 적어둔 SQL문장 실행
+		int cnt = 0; // 유니크(pk) 중복 방지(?)
+		try {
+			cnt = session.insert("join", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 3. 세션 반환
+		session.close();
+		
+		// 4. 결과 리턴
+		return cnt;
+	}
+	
+
+	// login 메서드
+	public MemberVO login(MemberVO vo) {
+
+		SqlSession session = sqlSessionFactory.openSession(true);
+		
+		MemberVO mvo = session.selectOne("login",vo);
+		
+		session.close();
+		return mvo;
+	}
+	
+	
+	
+}
