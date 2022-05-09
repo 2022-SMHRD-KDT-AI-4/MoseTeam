a678b0b6940e9fa7453169cb951c3864a61aea3f
smhrd <smhrd@220.93.165.167>
2022-05-09 10:30:56 +0900

    프로젝트 폴더 커밋 그냥 복붙이라 수정해서 사용~

---
+++ MenuRecommendation/src/main/java/MT/model/MemberVO.java
@@ -0,0 +1,12 @@
+package MT.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+public class MemberVO {
+
+
+}
