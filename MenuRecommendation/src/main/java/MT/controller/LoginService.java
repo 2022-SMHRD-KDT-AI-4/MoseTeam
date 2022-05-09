6aa98bba0d763fa4332b50da80a39cfba84a25ff
smhrd <smhrd@220.80.33.84>
2022-05-09 14:06:53 +0900

    이연진
    1. 기본파일(sql, config, MemberDAO, MemberMapper) 생성
    
    2. 회원가입 기능 완료
    
    3. 로그인 기능 미완(main.html에서 LoginService로 연결부분 미완)

--- MenuRecommendation/src/main/java/MT/controller/LoginService.java
+++ MenuRecommendation/src/main/java/MT/controller/LoginService.java
@@ -1,7 +1,6 @@
 package MT.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,16 +8,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import MT.model.MemberDAO;
+import MT.model.MemberVO;
 
 @WebServlet("/LoginService")
 public class LoginService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 
+		request.setCharacterEncoding("utf-8");
+		// 1. 파라미터 수집
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		MemberVO vo = new MemberVO(id, pw);
+		
+		// 2. DAO 메서드
+		MemberDAO dao = new MemberDAO();
+		MemberVO mvo = dao.login(vo);
+		
+		// 3. 로그인 성공/실패 판단
+		if(mvo==null) {
+			// 실패
+			System.out.println("로그인 실패");
+		}else {
+			// 성공
+			response.sendRedirect("join.html");
+			System.out.println("로그인 성공");
+			// 사용자의 정보 유지 --> Session 이용
+			HttpSession session = request.getSession();
+			session.setAttribute("vo", mvo);
+		}
 	}
 
 }
--- MenuRecommendation/src/main/java/MT/model/MemberVO.java
+++ MenuRecommendation/src/main/java/MT/model/MemberVO.java
@@ -6,7 +6,16 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+@Data // 기본 메서드 생성
+@AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자
+@NoArgsConstructor // 아무것도 들어있지 않은 생성자
+@RequiredArgsConstructor
 public class MemberVO {
 
+	@NonNull
+	private String id;
+	@NonNull
+	private String pw;
+	private String nickname;
 
 }
