c20dee26c2c7d4799a9d312730bcbd6478a75064
smhrd <smhrd@61.80.106.115>
2022-05-10 17:51:38 +0900

    COMU VIEW 생성 - 지균

--- MenuRecommendation/src/main/java/MT/model/sql.sql
+++ MenuRecommendation/src/main/java/MT/model/sql.sql
@@ -3,3 +3,27 @@ select * from member
 select * from member
 		where MEMBER_ID = 'test'
 		and MEMBER_PW = '1234'
+		
+		
+
+select * from BOARD
+
+alter table BOARD add FILE_NAME varchar(100) not null;
+
+
+alter table BOARD drop column filename;
+
+
+
+create or replace view COMU 
+ as select board_no
+		   ,board_title 
+		   ,board_content 
+		   ,member_id
+		   ,write_date 
+		   ,file_name
+		   from BOARD;
+		   
+		   
+		   
+select * from COMU
