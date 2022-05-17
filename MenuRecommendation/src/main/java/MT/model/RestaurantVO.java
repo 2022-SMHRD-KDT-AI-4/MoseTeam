abdc36cdea34a49c3523cae061917e5bd6f545b8
smhrd <smhrd@220.80.33.84>
2022-05-17 09:32:48 +0900

    이연진
    레스토랑 인기 count 기능 틀 제작
    reviewService
    RestaurantDAO
    RestaurantVO
    RestaurantService 수정

--- MenuRecommendation/src/main/java/MT/model/RestaurantVO.java
+++ MenuRecommendation/src/main/java/MT/model/RestaurantVO.java
@@ -5,16 +5,27 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class RestaurantVO {
+	@NonNull
 	private int rest_id;
+	@NonNull
 	private String rest_nm;
+	@NonNull
 	private String main_cate;
+	@NonNull
 	private String sub_cate;
+	@NonNull
 	private String rest_addr;
+	@NonNull
 	private String menu_id;
+	@NonNull
 	private List<RestmenuVO> restMenu;  
+	private int count;
 }
